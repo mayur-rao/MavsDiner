bfa3b4d2e70efabccf80daaecdb6dc2f230cd611
mayur-rao <mayurrao@gmail.com>
2016-02-26 21:04:59 -0600

    Initial Commit

---
+++ MavsDiner/app/src/main/java/com/group6/cse5324/mavsdiner/MainActivity.java
@@ -0,0 +1,13 @@
+package com.group6.cse5324.mavsdiner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
