610118252cbeed51fb3a0eb77346dbe5ef738fad
Vyshak Mulbagal Bheemarao <vyshak.mulbagalbheemar@mavs.uta.edu>
2016-04-06 00:02:10 -0500

    Testing

---
+++ app/src/main/java/com/example/mavsdiner/mavsdiner/Test_Vy.java
@@ -0,0 +1,13 @@
+package com.example.mavsdiner.mavsdiner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Test_Vy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test__vy);
+    }
+}
