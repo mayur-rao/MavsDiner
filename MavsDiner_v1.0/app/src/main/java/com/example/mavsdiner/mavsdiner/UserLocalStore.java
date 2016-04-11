586838c2bceda3d552062057d242fbdd00960ab1
Swaroop Raja Srinivas Setty <Swaroop@Swaroops-MacBook-Air.local>
2016-04-11 01:10:22 -0500

    new version

---
+++ MavsDiner_v1.0/app/src/main/java/com/example/mavsdiner/mavsdiner/UserLocalStore.java
@@ -0,0 +1,70 @@
+package com.example.mavsdiner.mavsdiner;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+/**
+ * Created by Swaroop on 4/9/16.
+ */
+
+public class UserLocalStore {
+    public static final String SP_NAME = "userDetails";
+    SharedPreferences userLocalDatabase;
+
+    public UserLocalStore(Context context)
+    {
+        System.out.println("Inside constructor");
+        userLocalDatabase = context.getSharedPreferences(SP_NAME, 0);
+    }
+
+    public void storeUserData(User user)
+    {
+        System.out.println("Commiting to user local store");
+        Log.d("UserLocalStore", user.email + user.password);
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putString("email", user.email);
+        spEditor.putString("password", user.password);
+        spEditor.putInt("customer_id", user.customer_id);
+        spEditor.commit();
+        System.out.println("Commited to user local store");
+    }
+
+    public User getLoggedInUser()
+    {
+
+        String email = userLocalDatabase.getString("email", "");
+        String pass = userLocalDatabase.getString("password", "");
+        int customer_id = userLocalDatabase.getInt("customer_id", -1);
+
+        User loggedInUser = new User(customer_id, email, pass);
+
+        return loggedInUser;
+    }
+
+    public void setUserLoggedIn(boolean flag)
+    {
+        System.out.println("Entered setUserLoggedIn"/* + " " +userLocalDatabase.getString("name", "")+ " " +userLocalDatabase.getString("uname", "")+ " " +userLocalDatabase.getString("pass", "")+ " " +userLocalDatabase.getString("age", "")*/);
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putBoolean("loggedIn", flag);
+        spEditor.commit();
+    }
+
+    public void clearUserData()
+    {
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.clear();
+    }
+
+    public boolean getUserLoggedIn()
+    {
+        if(userLocalDatabase.getBoolean("loggedIn", false) == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
